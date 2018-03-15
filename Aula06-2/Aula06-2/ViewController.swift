f52cc23e5ca9737e18f17e82efd4ef957e8418cc
HC2MAC017 <hc2mac017@iMac-2.local>
2018-03-14 21:32:40 -0300

    log ciclo de vida UIViewController

---
+++ Aula06-2/Aula06-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aula06-2
+//
+//  Created by HC2MAC017 on 14/03/18.
+//  Copyright © 2018 JJC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: VCLLoggingViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
