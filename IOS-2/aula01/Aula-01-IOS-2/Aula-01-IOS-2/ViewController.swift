bf3d32385758827d544cd55a39afe914e811cb84
HC2MAC017 <hc2mac017@iMac-2.local>
2018-03-21 21:23:42 -0300

    aula thread

--- IOS-2/aula01/Aula-01-IOS-2/Aula-01-IOS-2/ViewController.swift
+++ IOS-2/aula01/Aula-01-IOS-2/Aula-01-IOS-2/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let enderecoDaImagem = "https://usercontent2.hubstatic.com/7881737.jpg"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,64 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //        baixarImagem(doEndereco: enderecoDaImagem)
+        
+        indicator.startAnimating()
+        let concurrentQueue = DispatchQueue(label: "novaThread", attributes: .concurrent)
+        concurrentQueue.async(execute: baixarImagemClosure)
+        
+        
+    }
+    
+    var imagem : UIImage?{
+        didSet{
+            DispatchQueue.main.async(execute: exibirImagem)
+        }
+    }
 
+    private func baixarImagemClosure(){
+        
+        if let url = URL(string: enderecoDaImagem){
+            do{
+                let data = try Data(contentsOf: url)
+                //colocar a imagem na tela
+                imagem = UIImage(data: data)
+                
+            }catch{
+                debugPrint(error)
+            }
+        }
+    }
+    
+    private func exibirImagem(){
+        guard let img = self.imagem else{return}
+        imageView.image = img
+        indicator.stopAnimating()
+    }
+    
+    private func baixarImagem(doEndereco enderecoDaImagem: String){
+        let concurrentQueue = DispatchQueue(label: "novaThread", attributes: .concurrent)
+
+        concurrentQueue.async { [unowned self] in
+            
+            if let url = URL(string: enderecoDaImagem){
+                do{
+                    let data = try Data(contentsOf: url)
+                    //colocar a imagem na tela
+                    if let image = UIImage(data: data){
+                        DispatchQueue.main.async {
+                            self.imageView.image = image
+                        }
+                    }
+                }catch{
+                    debugPrint(error)
+                }
+            }
+        }
+    }
+    
 }
 
