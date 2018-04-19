16d5ae78bd1efdb05dac130ee1551da3449235a6
HC2MAC017 <hc2mac017@imac-8.local>
2018-04-18 22:45:18 -0300

    Aula 05 - auto layout e size class

---
+++ IOS-2/aula05/Aula05/Aula05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aula05
+//
+//  Created by HC2MAC017 on 18/04/2018.
+//  Copyright © 2018 JJC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
